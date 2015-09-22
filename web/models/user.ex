7651ac0ef4a90e51b71e3e8ec2a675915ef0c481
Max Beizer <max.beizer@gmail.com>
2015-09-21 20:25:11 -0500

    Validating create

--- web/models/user.ex
+++ web/models/user.ex
@@ -12,11 +12,14 @@ defmodule Support.User do
     timestamps
   end
 
-  @required_fields ~w(name email encrypted_password)
+  @required_fields ~w(name email password password_confirmation)
   @optional_fields ~w()
 
   def changeset(model, :register, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> validate_length(:name, min: 1, message: "cannot be blank")
+    |> validate_length(:email, min: 1, message: "cannot be blank")
+    |> validate_format(:eamil, ~r/@/)
   end
 end
